28dbe4c8ab2114f4c67c9cdb224329e35e208572
James Z <125828382+Jamziewebdev@users.noreply.github.com>
2025-10-29 10:44:35 +0100

    Added about and review basics

---
+++ gronagaffeln/src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export function About () {
+
+    return (
+        <>
+            <div className="area-header">            
+                <h1>Om oss</h1>
+            </div>
+            <div className="about-content">
+                <p>Vi är en restaurang med god vegetarisk mat för alla tillfällen!</p>
+            </div>
+        </>
+    )
+}
