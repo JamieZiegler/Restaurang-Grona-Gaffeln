e7e4b64259ff7361f8521ebd94c6f9d21ab2c302
James Z <125828382+Jamziewebdev@users.noreply.github.com>
2025-10-29 10:04:14 +0100

    Added basic structure to Home.jsx and App.jsx

--- gronagaffeln/src/App.jsx
+++ gronagaffeln/src/App.jsx
@@ -1,11 +1,15 @@
-
+import { Footer } from './components/Footer'
+import { Header } from './components/Header'
+import { Home } from './components/Home'
 
 function App() {
 
 
   return (
     <>
-
+      <Header></Header>
+      <Home></Home>
+      <Footer></Footer>
     </>
   )
 }
