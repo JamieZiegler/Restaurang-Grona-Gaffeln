3b9a9076c6b264cc0df078b321390befd0f3c986
Rebeccagh <rebecca.gren.hornfeldt.webd24jon@edu.tucsweden.se>
2025-10-29 10:47:24 +0100

    Menu component

---
+++ gronagaffeln/src/components/Menu.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export function Menu() {
+    return (
+    <>
+        <div>
+            <h3>Rostad blomkålssoppa med tryffelolja</h3>
+            <p>Serveras med ett knaprigt örtfocaccia.</p>
+            <p>149kr</p>
+        </div>
+        <div>
+            <h3>Svamprisotto med parmesan och timjan</h3>
+            <p>Krämig arborioris med kantareller, portabello och en skvätt vitt vin.</p>
+            <p>169kr</p>
+        </div>
+        <div>
+            <h3>Indisk kikärtsgryta (Chana Masala)</h3>
+            <p>Serveras med jasminris, naanbröd och en klick yoghurt.</p>
+            <p>155kr</p>
+        </div>
+        <div>
+            <h3>Lasagne med grillade grönsaker och linser</h3>
+            <p>Linsragu, zucchini, paprika, bechamelsås och mozzarella.</p>
+            <p>169kr</p>
+        </div>
+        <div>
+            <h3>Pannacotta på kokosmjölk med passionsfrukt</h3>
+            <p>Laktosfri och exotiskt len.</p>
+            <p>99kr</p>
+        </div>
+    </>
+    )
+}
