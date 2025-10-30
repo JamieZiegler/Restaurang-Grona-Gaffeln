0dd5078d26427405c5b2e1923653a2f2b4f95f26
James Z <125828382+Jamziewebdev@users.noreply.github.com>
2025-10-30 15:45:42 +0100

    Skapat "om oss" enl. kravlista

--- gronagaffeln/src/components/About.jsx
+++ gronagaffeln/src/components/About.jsx
@@ -1,17 +1,29 @@
 import React from "react";
+import sallad from "../assets/about/sallad.png";
+import '../about.css';
 
 export function About () {
 
     return (
         <>
-            <section>
-                <div className="about-section">
-                    <div className="area-header">            
-                        <h1>Om oss</h1>
+            <section id="about-page">
+                <div className="about-container">
+                    <div className="about-information">            
+                        <h1>Hållbarhet i fokus</h1>
+                        <h2>Vi är en restaurang med god vegetarisk mat för alla tillfällen!</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ut eius aliquid consectetur, corrupti architecto ipsum inventore. Molestias nostrum minima nulla quas. Hic numquam eaque in atque consequatur, harum reiciendis?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, expedita asperiores. Totam commodi officiis, modi maxime, esse distinctio nisi magni veniam doloremque ex earum, quia error? Rerum assumenda esse labore.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, doloremque. Repudiandae, voluptatibus? Quod, doloremque. Quasi, doloremque. Repudiandae, voluptatibus? Quod, doloremque.</p>
                     </div>
-                    <div className="about-content">
-                        <p>Vi är en restaurang med god vegetarisk mat för alla tillfällen!</p>
+                    <div className="about-image">
+                        <img src={sallad} alt="En fräsch sallad med olika grönsaker" />
                     </div>
+                    <div className="cta-about">
+                        <button className="aboutBtn">Läs mer om oss</button>
+                        <button className="ytBtn">Besök vår YouTube</button>
+                    </div>
+
+                    
                 </div>
             </section>
         </>
